016acf22532be90cf9bcabb7f5b1623c1fd7161b
lodmik <kimdol@proton.me>
2023-01-25 10:31:01 -0800

    Fix more linter complaints

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,15 +3,15 @@ const glob = require('glob');
 
 const { REST, Routes } = require('discord.js');
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 const commands = [];
 // Grab all the command files from the commands directory
 const commandFiles = glob.sync('**/*.js', { cwd: './commands' });
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
 }
 // TODO find out how to stop index.js from executing when it is imported in command files
 
@@ -21,23 +21,23 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 // Delete commands
 // guild-based commands
 rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.TEST_GUILD_ID), { body: [] })
-	.then(() => console.log('Successfully deleted all guild commands.'))
-	.catch(console.error);
+    .then(() => console.log('Successfully deleted all guild commands.'))
+    .catch(console.error);
 // global commands
 rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] })
-	.then(() => console.log('Successfully deleted all application commands.'))
-	.catch(console.error);
+    .then(() => console.log('Successfully deleted all application commands.'))
+    .catch(console.error);
 
 // and deploy your commands!
 (async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
         
         // The put method is used to fully refresh all commands in the guild with the current set
 
         let data;
         // Deploy commands to all guilds
-        if (args[0] == "all") {
+        if (args[0] == 'all') {
             data = await rest.put(
                 Routes.applicationCommands(process.env.CLIENT_ID),
                 { body: commands },
@@ -53,8 +53,8 @@ rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] })
             console.log(`Successfully reloaded ${data.length} application (/) commands.`);
         }
 		
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
-	}
+    } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error);
+    }
 })();
