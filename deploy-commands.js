084ddc53abced8ae91b136a819e893866fa8ea8f
lodmik <kimdol@proton.me>
2023-01-24 20:03:00 -0800

    Add comment deletion before registration in command deployment

--- deploy-commands.js
+++ deploy-commands.js
@@ -17,6 +17,16 @@ for (const file of commandFiles) {
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
+// Delete commands
+// guild-based commands
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.TEST_GUILD_ID), { body: [] })
+	.then(() => console.log('Successfully deleted all guild commands.'))
+	.catch(console.error);
+// global commands
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] })
+	.then(() => console.log('Successfully deleted all application commands.'))
+	.catch(console.error);
+
 // and deploy your commands!
 (async () => {
 	try {
