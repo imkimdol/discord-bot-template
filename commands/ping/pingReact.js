01201aaa7af6ee4dd4847216cc7972d216ecdac8
lodmik <kimdol@proton.me>
2023-01-25 10:54:47 -0800

    Small change

--- commands/ping/pingReact.js
+++ commands/ping/pingReact.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping_react')
-        .setDescription('Replies with \'Pong!\' and reacts to the reply with \'😄\'.'),
+        .setDescription('Replies with \'Pong!\' and reacts to the reply with 😄.'),
     async execute(interaction) {
         const message = await interaction.reply({ content: 'Pong!', fetchReply: true });
         message.react('😄');
