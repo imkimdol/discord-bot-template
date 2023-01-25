4252e7ee7fa7cbaac302841fe1b93f7e6d2dd742
lodmik <kimdol@proton.me>
2023-01-25 10:30:26 -0800

    Add restricted and latency command

---
+++ commands/admin/restricted.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+require('dotenv').config();
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('restricted')
+        .setDescription('A restricted command.')
+        .setDMPermission(false),
+    async execute(interaction) {
+        if (interaction.user.id === process.env.OWNER_ID) {
+            await interaction.reply({ content: 'Command successful.', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'You are not permitted to use this command.', ephemeral: true });
+        }
+    },
+};
