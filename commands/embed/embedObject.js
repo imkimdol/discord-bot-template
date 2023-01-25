efe88d4b8e11082bc241d905d4d0ba3c2b34662c
lodmik <kimdol@proton.me>
2023-01-25 15:43:57 -0800

    Add embed commands

---
+++ commands/embed/embedObject.js
@@ -0,0 +1,59 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('embed_object')
+        .setDescription('Replies with an embed example using an embed object.'),
+    async execute(interaction) {
+        const exampleEmbed = {
+            color: 0x0099ff,
+            title: 'Some title',
+            url: 'https://discord.js.org',
+            author: {
+                name: 'Some name',
+                icon_url: 'https://i.imgur.com/AfFp7pu.png',
+                url: 'https://discord.js.org',
+            },
+            description: 'Some description here',
+            thumbnail: {
+                url: 'https://i.imgur.com/AfFp7pu.png',
+            },
+            fields: [
+                {
+                    name: 'Regular field title',
+                    value: 'Some value here',
+                },
+                {
+                    name: '\u200b',
+                    value: '\u200b',
+                    inline: false,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+            ],
+            image: {
+                url: 'https://i.imgur.com/AfFp7pu.png',
+            },
+            timestamp: new Date().toISOString(),
+            footer: {
+                text: 'Some footer text here',
+                icon_url: 'https://i.imgur.com/AfFp7pu.png',
+            },
+        };        
+    
+        await interaction.reply({ embeds: [exampleEmbed] });
+    },
+};
