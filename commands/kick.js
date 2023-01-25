e942f173622f3d85131476ed7ac03383b4bdddf8
lodmik <kimdol@proton.me>
2023-01-24 20:05:09 -0800

    Change kick command such that it exports

--- commands/kick.js
+++ commands/kick.js
@@ -1,14 +1,17 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
-const data = new SlashCommandBuilder()
-	.setName('kick')
-	.setDescription('Select a member and kick them.')
-	.addUserOption(option =>
-		option
-			.setName('target')
-			.setDescription('The member to kick')
-			.setRequired(true))
-	.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers | PermissionFlagsBits.KickMembers)
-    .setDMPermission(false);
-
-	// TODO Add an execute function
+module.exports = {
+    data: new SlashCommandBuilder()
+		.setName('kick')
+		.setDescription('Select a member and kick them.')
+		.addUserOption(option =>
+			option
+				.setName('target')
+				.setDescription('The member to kick')
+				.setRequired(true))
+		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers | PermissionFlagsBits.KickMembers)
+		.setDMPermission(false),
+    async execute(interaction) {
+		// TODO Add an execute function
+	},
+};
